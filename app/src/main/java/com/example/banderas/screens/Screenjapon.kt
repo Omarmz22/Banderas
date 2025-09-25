56168423fd051c5d882d082d569081dd4b6ee77e
Omarmz22 <24170159@uttcampus.edu.mx>
2025-09-24 20:22:56 -0600

    Acomode a Japon en su rama

--- app/src/main/java/com/example/banderas/screens/Screenjapon.kt
+++ app/src/main/java/com/example/banderas/screens/Screenjapon.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
-
 @Preview()
 @Composable
 fun Screenjapon(){
