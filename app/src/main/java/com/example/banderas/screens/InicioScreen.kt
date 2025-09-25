8d85db3e0ddc10a1163bd1ca845cb38d56efd75f
Omarmz22 <24170159@uttcampus.edu.mx>
2025-09-25 12:45:37 -0600

    avance en brasil

--- app/src/main/java/com/example/banderas/screens/InicioScreen.kt
+++ app/src/main/java/com/example/banderas/screens/InicioScreen.kt
@@ -2,47 +2,40 @@ package com.example.banderas.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+
 @Preview
 @Composable
-fun BnaderaChile(){
-    Column(modifier = Modifier){
-        Row(modifier = Modifier.weight(1f).fillMaxWidth()){
-            Box(
-            contentAlignment = Alignment.Center,
+fun BnaderaBrazil(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Green)
+    ) {
+        Box(
             modifier = Modifier
-                .weight(1f)
-                .fillMaxHeight()
-                .background(Color.Blue)
-            ){
-                Text(
-                    text = "★",
-                    color = Color.White,
-                    fontSize = 50.sp
-                )
-           }
-            Box(
-                modifier = Modifier
-                    .weight(2f)
-                    .fillMaxHeight()
-                    .background(Color.White)
-            )
-        }
-        Row(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth()
-                .background(Color.Red)
-        ){}
+                .size(300.dp)
+                .drawWithCache {
+                    val path = Path().apply {
+                        moveTo(size.width / 2f, 0f) // Top center
+                        lineTo(size.width, size.height / 2f) // Middle right
+                        lineTo(size.width / 2f, size.height) // Bottom center
+                        lineTo(0f, size.height / 2f) // Middle left
+                        close()
+                    }
+                    onDrawBehind {
+                        drawPath(path, color = Color.Yellow)
+                    }
+                }
+        )
     }
+
 }
