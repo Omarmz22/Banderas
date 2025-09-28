0bc2d2a3fb0d1b32df80b19b09d1bd6c167cb351
Omarmz22 <24170159@uttcampus.edu.mx>
2025-09-28 14:30:00 -0600

    Bandera de Brazil

--- app/src/main/java/com/example/banderas/MainActivity.kt
+++ app/src/main/java/com/example/banderas/MainActivity.kt
@@ -8,8 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.banderas.screens.InicioScreen
-import com.example.banderas.screens.Screenjapon
+import com.example.banderas.screens.BnaderaBrazil
 import com.example.banderas.ui.theme.BanderasTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +16,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Screenjapon()
+            BnaderaBrazil()
         }
     }
 }
--- app/src/main/java/com/example/banderas/screens/InicioScreen.kt
+++ app/src/main/java/com/example/banderas/screens/InicioScreen.kt
@@ -1,12 +1,15 @@
 package com.example.banderas.screens
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,24 +21,31 @@ fun BnaderaBrazil(){
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Green)
+            .background(Color.Green),
+        contentAlignment = Alignment.Center
     ) {
-        Box(
-            modifier = Modifier
-                .size(300.dp)
-                .drawWithCache {
-                    val path = Path().apply {
-                        moveTo(size.width / 2f, 0f) // Top center
-                        lineTo(size.width, size.height / 2f) // Middle right
-                        lineTo(size.width / 2f, size.height) // Bottom center
-                        lineTo(0f, size.height / 2f) // Middle left
-                        close()
-                    }
-                    onDrawBehind {
-                        drawPath(path, color = Color.Yellow)
-                    }
-                }
-        )
+        Canvas(modifier = Modifier.size(300.dp)) {
+            val canvasWidth = size.width
+            val canvasHeight = size.height
+
+            val path = Path().apply {
+                moveTo(canvasWidth / 2, 0f)
+                lineTo(canvasWidth, canvasHeight / 2f)
+                lineTo(canvasWidth / 2, canvasHeight)
+                lineTo(0f, canvasHeight / 2f)
+                close()
+            }
+            drawPath(
+                path = path,
+                color = Color.Yellow
+            )
+            drawCircle(
+                color = Color.Blue,
+                radius = canvasWidth * 0.25f,
+                center = Offset(canvasWidth / 2, canvasHeight / 2)
+            )
+
+        }
     }
 
 }
