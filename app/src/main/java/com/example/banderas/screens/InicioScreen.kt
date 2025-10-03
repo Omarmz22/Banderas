0fe826fb5a5be3f49804b017d83d05bc9b005604
Omarmz22 <24170159@uttcampus.edu.mx>
2025-10-02 22:02:08 -0600

    bandera de chile constrian layout

--- app/src/main/java/com/example/banderas/screens/InicioScreen.kt
+++ app/src/main/java/com/example/banderas/screens/InicioScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -13,31 +15,69 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.example.banderas.R
 
 @Composable
 @Preview
-fun InicioScreen() {
-    ConstraintLayout(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.White)
-    )
-    {
-        val(circuloRojo) = createRefs()
+fun InicioScreen(){
+    ConstraintLayout (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ){
+        val (rayaBlanca, rayaRoja, cuadroAzul, estrella) = createRefs()
 
         Box(
             modifier = Modifier
-                .size(150.dp)
-                .background(Color(0xFFBC002D), shape = CircleShape)
-                .constrainAs(circuloRojo) {
+                .constrainAs(rayaBlanca){
+                    top.linkTo(parent.top)
+                    start.linkTo(cuadroAzul.end)
+                    end.linkTo(parent.end)
+                    height = Dimension.percent(0.5f)
+                    width = Dimension.fillToConstraints
+                }
+                .background(Color.White)
+        )
+        Box(
+            modifier = Modifier
+                .constrainAs(rayaRoja){
+                    top.linkTo(rayaBlanca.bottom)
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                    top.linkTo(parent.top)
                     bottom.linkTo(parent.bottom)
+                    width = Dimension.percent(1f)
+                    height = Dimension.percent(0.5f)
                 }
+                .background(Color.Red)
+        )
+
+        Box(
+            modifier = Modifier
+                .constrainAs(cuadroAzul){
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(rayaBlanca.start)
+                    width = Dimension.percent(0.40f)
+                    height = Dimension.percent(0.5f)
+                }
+                .background(Color.Blue)
+        )
+
+        Box(
+            modifier = Modifier
+                .size(30.dp)
+                .background(Color.White, shape = CircleShape)
+                .constrainAs(estrella){
+                    start.linkTo(cuadroAzul.start)
+                    end.linkTo(cuadroAzul.end)
+                    top.linkTo(cuadroAzul.top)
+                    bottom.linkTo(cuadroAzul.bottom)
+
+                }
+
         )
     }
-
 }
 
 
