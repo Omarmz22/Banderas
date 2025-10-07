3491877b75af63bb8b8fb243c99cbf7d2b5ccbb5
Omarmz22 <24170159@uttcampus.edu.mx>
2025-10-06 19:25:02 -0600

    SPIDERMAN

--- app/src/main/java/com/example/banderas/screens/InicioScreen.kt
+++ app/src/main/java/com/example/banderas/screens/InicioScreen.kt
@@ -1,83 +1,62 @@
 package com.example.banderas.screens
-
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
-import com.example.banderas.R
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
-@Composable
 @Preview
-fun InicioScreen(){
-    ConstraintLayout (
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-    ){
-        val (rayaBlanca, rayaRoja, cuadroAzul, estrella) = createRefs()
+@Composable
+fun PixelArtComposable() {
+    val pixelMatrix = arrayOf(
+        arrayOf("#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#000000", "#000000", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#000000", "#000000", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#000000", "#000000", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#000000", "#000000", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#000000", "#000000", "#FFFFFF", "#FFFFFF", "#000000", "#000000", "#FFFFFF", "#000000","#000000", "#FFFFFF", "#000000", "#000000", "#FFFFFF", "#FFFFFF", "#000000", "#000000", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#000000", "#000000", "#000000", "#000000", "#FFFFFF", "#000000", "#000000", "#000000","#000000", "#000000", "#000000", "#FFFFFF", "#000000", "#000000", "#000000", "#000000", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#000000", "#000000", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#000000", "#000000","#000000", "#000000", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#000000", "#000000", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#000000", "#FFFFFF", "#000000", "#000000", "#000000","#000000", "#000000", "#000000", "#FFFFFF", "#000000", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#000000", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#000000", "#000000","#000000", "#000000", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#000000", "#FFFFFF"),
+        arrayOf("#FFFFFF","#000000", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#000000", "#000000","#000000", "#000000", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#000000", "#FFFFFF"),
+        arrayOf("#FFFFFF","#000000", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#000000", "#000000","#000000", "#000000", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#000000", "#FFFFFF"),
+        arrayOf("#FFFFFF","#000000", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#000000","#000000", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#000000", "#FFFFFF"),
+        arrayOf("#FFFFFF","#000000", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#000000","#000000", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#000000", "#FFFFFF"),
+        arrayOf("#FFFFFF","#000000", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#000000", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#000000", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#000000", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF","#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
+        arrayOf("#FFFFFF","#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#000000", "#FFFFFF","#FFFFFF", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"),
 
-        Box(
-            modifier = Modifier
-                .constrainAs(rayaBlanca){
-                    top.linkTo(parent.top)
-                    start.linkTo(cuadroAzul.end)
-                    end.linkTo(parent.end)
-                    height = Dimension.percent(0.5f)
-                    width = Dimension.fillToConstraints
+    )
+
+    Column {
+        for (row in pixelMatrix) {
+            Row {
+                for (colorHex in row) {
+                    var currentColor by remember { mutableStateOf(Color(android.graphics.Color.parseColor(colorHex))) }
+
+                    Box(
+                        modifier = Modifier
+                            .size(20.dp)
+                            .background(currentColor)
+                            .border(1.dp, Color.Black)
+                            .clickable {
+                                currentColor = Color.Yellow
+                            }
+                    )
                 }
-                .background(Color.White)
-        )
-        Box(
-            modifier = Modifier
-                .constrainAs(rayaRoja){
-                    top.linkTo(rayaBlanca.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                    bottom.linkTo(parent.bottom)
-                    width = Dimension.percent(1f)
-                    height = Dimension.percent(0.5f)
-                }
-                .background(Color.Red)
-        )
-
-        Box(
-            modifier = Modifier
-                .constrainAs(cuadroAzul){
-                    top.linkTo(parent.top)
-                    start.linkTo(parent.start)
-                    end.linkTo(rayaBlanca.start)
-                    width = Dimension.percent(0.40f)
-                    height = Dimension.percent(0.5f)
-                }
-                .background(Color.Blue)
-        )
-
-        Box(
-            modifier = Modifier
-                .size(30.dp)
-                .background(Color.White, shape = CircleShape)
-                .constrainAs(estrella){
-                    start.linkTo(cuadroAzul.start)
-                    end.linkTo(cuadroAzul.end)
-                    top.linkTo(cuadroAzul.top)
-                    bottom.linkTo(cuadroAzul.bottom)
-
-                }
-
-        )
+            }
+        }
     }
 }
-
-
